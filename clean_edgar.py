ff4c891d6b46a9b4cb56ee59e2945abbcd4802bd
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-04-01 12:53:59 -0500

    load into one file for same year

--- clean_edgar.py
+++ clean_edgar.py
@@ -35,9 +35,7 @@ def process_day(path, date_dir, day_file, results_path):
     zf = zipfile.ZipFile(path_day)
     zp_list = zipfile.ZipFile.namelist(zf)
     csv_regex = re.compile('(.*)\.csv')
-    result_dir = results_path + '/' + date_dir
-    if not os.path.exists(result_dir):
-        os.mkdir(result_dir)
+    df = pd.DataFrame(data={})
     for file in zp_list:
         matcher = csv_regex.match(file)
         if (matcher):
@@ -47,30 +45,30 @@ def process_day(path, date_dir, day_file, results_path):
             
             df = df[(df.crawler == np.float64(0)) & (df.idx == np.float64(0)) & (df.code < error_code_limit)]
             df = df.drop(columns=['zone','code','size','idx','norefer','noagent','find','crawler','browser'])
-            print(df.head())
             print("after removeing crawerls, index, codes:" + str(df.size))
             
             downloads_count = df.ip.value_counts()
             downloads_count = downloads_count[downloads_count<threshold].index
             df = df[df.ip.isin(downloads_count)]
             print("after removing robots:" + str(df.size))
-            df.to_csv(result_dir + '/' + matcher.group(1) + '.csv.gzip', compression='gzip', index=False)
+    return df
             
 
 
 def process_data(path, results_path):
-    print('path=' + path)
-    print('results_path=' + results_path)
     date_dirs = [f for f in listdir(path) if isdir(join(path, f))]
     regex_dir = re.compile('([0-9]{4})')
     regex_zip = re.compile('log([0-9]{4})([0-9]{2})([0-9]{2}).zip')
     for date_dir in date_dirs:
         if (regex_dir.match(date_dir)):
+            df = pd.DataFrame(data={})
             for day_file in listdir(path + date_dir):
                 if regex_zip.match(day_file):
-                       process_day(path, date_dir, day_file, results_path)
-
-
+                       df = df.append(process_day(path, date_dir, day_file, results_path))
+            
+            print("saving file:" + str(df.size))
+            df.to_csv(results_path + '/' + date_dir + '.csv.gzip', compression='gzip', index=False)
+            
 #process_data('/Users/mikaelapisanileal/Documents/edgar/edger-cleaner/', '/Users/mikaelapisanileal/Documents/edgar/edger-cleaner/results')
 
 
