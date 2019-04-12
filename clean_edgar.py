01b65aaa9f77e257ec7c1874ca06fedbe8d725d6
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-04-12 14:27:53 -0500

    add merge with accession number

--- clean_edgar.py
+++ clean_edgar.py
@@ -30,9 +30,8 @@ output_size_gb = 5*10000000000
     #   remove robots:  based on the number of unique firms that a given IP address
     #                   downloads on a given day. if it is more than the threshold is a robot.
     #
-    # columns to keep:ip,date,time,cik,accession(mapping with form type and date),extention
-          
-def process_day(path, date_dir, day_file, results_path):
+    # columns to keep:ip,date,time,cik,accession(mapping with form type and date),extention         
+def process_day(path, date_dir, day_file):
     path_day = path + date_dir + '/' + day_file
     zf = zipfile.ZipFile(path_day)
     zp_list = zipfile.ZipFile.namelist(zf)
@@ -54,34 +53,62 @@ def process_day(path, date_dir, day_file, results_path):
             df = df[df.ip.isin(downloads_count)]
             print("after removing robots:" + str(df.size))
     return df
-            
-def save_csv(df, results_path, date_dir):
+ 
+#save data into csv of chunk size defined (GB)         
+def save_csv(df, results_path, year):
     row_size = sum([df.memory_usage()[1] for pair in df.memory_usage().iteritems()])
     chunks = math.trunc(row_size/output_size_gb)
     if chunks>0:
         idx = 0
         for chunk in np.array_split(df, chunks):
-            chunk.to_csv(results_path + '/' + date_dir + '_' + str(idx) + '.csv', index=False)
+            chunk.to_csv(results_path + '/' + year + '_' + str(idx) + '.csv', index=False)
             idx=+1
     else:
-        df.to_csv(results_path + '/' + date_dir + '_0.csv', index=False)
+        df.to_csv(results_path + '/' + year + '_0.csv', index=False)
+
+
+#load data from master files and clean it
+def load_master(master_path, year):
+    master_path = '/Volumes/TOSHIBA EXT/master index files/'
+    year = '2014'
+    masters = pd.DataFrame(data={})
+    regex_file = re.compile('master' + year + '.*')
+   
+    for master_file in listdir(master_path):
+        if (regex_file.match(master_file)):
+            master = pd.read_csv(master_path + master_file, skiprows=11,
+                names=['CIK','Company Name', 'Form Type', 'Date Filed', 'Filename'],
+                sep='|')
+            masters = masters.append(master)
     
-def process_data(path, results_path):
+    #modify filename data to keep accession number
+    masters['Filename'] = masters['Filename'].apply(lambda x: x.split('/')[3].replace('.txt', ''))
+    #keep columns of interest
+    masters = masters[['Filename', 'CIK', 'Form Type','Date Filed']]
+    return masters
+
+#for each date folder, process days files and append into one dataframe
+#save data into csv files
+def process_data(path):
     date_dirs = [f for f in listdir(path) if isdir(join(path, f))]
     regex_dir = re.compile('([0-9]{4})')
     regex_zip = re.compile('log([0-9]{4})([0-9]{2})([0-9]{2}).zip')
-    for date_dir in date_dirs:
-        if (regex_dir.match(date_dir)):
+    for year in date_dirs:
+        if (regex_dir.match(year)):
             df = pd.DataFrame(data={})
-            for day_file in listdir(path + date_dir):
+            for day_file in listdir(path + year):
                 if regex_zip.match(day_file):
-                       df = df.append(process_day(path, date_dir, day_file, results_path))
-            print("saving data:" + str(df.size))
-            save_csv(df, results_path, date_dir)
+                       df = df.append(process_day(path, year, day_file))
+    return df
             
-            
-#process_data('/Users/mikaelapisanileal/Documents/edgar/edger-cleaner/', '/Users/mikaelapisanileal/Documents/edgar/edger-cleaner/results')
-
+def process_year(path, results_path, year):          
+    masters = load_master(path, year)
+    df = process_data('/Users/mikaelapisanileal/Documents/edgar/edger-cleaner/')
+    data_merged = pd.merge(df, masters, how='inner', left_on=['accession', 'cik'], 
+                           right_on=['Filename', 'CIK'])
+    data_merged = data_merged[['ip', 'date', 'time', 'cik', 'accession', 
+                               'extention', 'Form Type','Date Filed']]
+    save_csv(data_merged, results_path, year)
 
 def main(argv):
     
