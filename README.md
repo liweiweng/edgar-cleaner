14c6416f76cb7195e0ae50d1545189779871bc2a
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-04-22 16:00:40 -0500

    add dropbox upload code

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# edger-cleaner
-Clean EDGER data
+# Edger-cleaner
+Python script for cleaning EDGER data
+
+## Virtualenv
+python3 -m v
+
+## Install requirements
+source 
+pip install --upgrade pip
+pip install -r requirements.txt
+
+## Execution
+
+python3 clean_edgar.py -p '/Volumes/TOSHIBA EXT/rawEDGAR/' -r '/Users/mikaelapisanileal/Documents/SASUniversityEdition/myfolders' -y '2014'
+
+## Cleaning
+1. Remove crawerls
+2. Remove index
+3. Remove codes >=error_code_limit
+4. Remove robots:  based on the number of unique firms that a given IP address downloads on a given day.
+                   If it is more than the threshold is a robot.
+
+## Columns from result
+- ip:
+- date:
+- time:
+- cik:
+- accession:
+- extention:
+- Form Type:
+- Date Filed:
+
+
+    
+
+
--- clean_edgar.py
+++ clean_edgar.py
@@ -8,19 +8,40 @@ Created on Mon Mar 25 16:01:57 2019
 """
 
 import pandas as pd
-import zipfile
 import numpy as np
+import os
 from os import listdir
 from os.path import isdir, join
 import re
 import sys
 import getopt
 import math
-
+import datetime
+import dropbox
+if sys.version_info >= (3, 6):
+    import zipfile
+else:
+    import zipfile36 as zipfile
 
 threshold = 50
 error_code_limit = 300
 output_size_gb = 5*10000000000
+access_token = "dhZNW6DTN2AAAAAAAAAC2ebJv8jOEPyPNb31b0cf7EtbeVqq8YpRPmiKjLxVT099"
+dropbox_folder = "/edgar_test/"
+
+#Transfer data to dropbox
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
 
 #function to process one file
 #data cleaning process
@@ -41,11 +62,13 @@ def process_day(path, date_dir, day_file):
         matcher = csv_regex.match(file)
         if (matcher):
             df = pd.read_csv(zf.open(file))
+            # df = pd.read_csv(zf.open(file), dtype={'ip':str, 'date':str, 'time':str, 'zone':str, 'cik':str, 'accession':str, 'extention':str, 'code':str,
+       #'size':str, 'idx':str, 'norefer':str, 'noagent':str, 'find':str, 'crawler':str, 'browser':str})
             print("Processing day: " + file)
             print("original size:" + str(df.size))
             
             df = df[(df.crawler == np.float64(0)) & (df.idx == np.float64(0)) & (df.code < error_code_limit)]
-            df.drop(columns=['zone','code','size','idx','norefer','noagent','find','crawler','browser'], inplace=True)
+            df = df[['ip', 'date', 'time', 'cik', 'accession', 'extention']]
             print("after removeing crawerls, index, codes:" + str(df.size))
             
             downloads_count = df.ip.value_counts()
@@ -58,19 +81,26 @@ def process_day(path, date_dir, day_file):
 def save_csv(df, results_path, year):
     row_size = sum([df.memory_usage()[1] for pair in df.memory_usage().iteritems()])
     chunks = math.trunc(row_size/output_size_gb)
+    transferData = TransferData(access_token)
     if chunks>0:
         idx = 0
         for chunk in np.array_split(df, chunks):
-            chunk.to_csv(results_path + '/' + year + '_' + str(idx) + '.csv', index=False)
+            file_from = results_path + '/' + year + '_' + str(idx) + '.csv'
+            file_to = dropbox_folder + year + '_' + str(idx) + '.csv'
+            chunk.to_csv(file_from, index=False)
+            transferData.upload_file(file_from, file_to)
+            os.remove(file_from)
             idx=+1
     else:
-        df.to_csv(results_path + '/' + year + '_0.csv', index=False)
+        file_from = results_path + '/' + year + '_0.csv'
+        file_to = dropbox_folder + year + '_0.csv'
+        df.to_csv(file_from, index=False)
+        transferData.upload_file(file_from, file_to)
+        os.remove(file_from)
 
 
 #load data from master files and clean it
 def load_master(master_path, year):
-    master_path = '/Volumes/TOSHIBA EXT/master index files/'
-    year = '2014'
     masters = pd.DataFrame(data={})
     regex_file = re.compile('master' + year + '.*')
    
@@ -87,50 +117,64 @@ def load_master(master_path, year):
     masters = masters[['Filename', 'CIK', 'Form Type','Date Filed']]
     return masters
 
-#for each date folder, process days files and append into one dataframe
-#save data into csv files
-def process_data(path):
-    date_dirs = [f for f in listdir(path) if isdir(join(path, f))]
-    regex_dir = re.compile('([0-9]{4})')
+ 
+def process_year(path, master_path, results_path, year):
+    df = pd.DataFrame(data={})
     regex_zip = re.compile('log([0-9]{4})([0-9]{2})([0-9]{2}).zip')
-    for year in date_dirs:
-        if (regex_dir.match(year)):
-            df = pd.DataFrame(data={})
-            for day_file in listdir(path + year):
-                if regex_zip.match(day_file):
-                       df = df.append(process_day(path, year, day_file))
-    return df
-            
-def process_year(path, results_path, year):          
-    masters = load_master(path, year)
-    df = process_data('/Users/mikaelapisanileal/Documents/edgar/edger-cleaner/')
+    for day_file in listdir(path + year):
+        if regex_zip.match(day_file):
+            df = df.append(process_day(path, year, day_file))          
+    masters = load_master(master_path, year)
     data_merged = pd.merge(df, masters, how='inner', left_on=['accession', 'cik'], 
                            right_on=['Filename', 'CIK'])
     data_merged = data_merged[['ip', 'date', 'time', 'cik', 'accession', 
                                'extention', 'Form Type','Date Filed']]
     save_csv(data_merged, results_path, year)
 
+#for each date folder, process days files and append into one dataframe
+#save data into csv files
+def process_data(path, master_path, results_path):
+    date_dirs = [f for f in listdir(path) if isdir(join(path, f))]
+    regex_dir = re.compile('([0-9]{4})')
+    for year in date_dirs:
+        if (regex_dir.match(year)):
+            before = datetime.datetime.now()
+            process_year(path, master_path, year)
+            after = datetime.datetime.now()
+            print('time elapsed for ' + year + ':' + str(after-before))
+           
+
+def info():
+    print('clean_edgar.py -p <path> -r <results_path>')
+
 def main(argv):
-    
    path = ''
+   master_path = ''
    results = ''
+   year = ''
    try:
-      opts, args = getopt.getopt(argv,'hp:r:',['path=','results='])
+      opts, args = getopt.getopt(argv,'hp:m:r:y:',['path=','master=','results=', 'year='])
    except getopt.GetoptError:
-      print ('clean_edgar.py -p <path> -r <results_path>')
+      info()
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print ('clean_edgar.py -p <path> -r <results_path>')
+         info()
          sys.exit()
       elif opt in ('-p', '--path'):
          path = arg
+      elif opt in ('-m', '--master'):
+         master_path = arg
       elif opt in ('-r', '--results'):
          results = arg
-       
-   process_data(path, results)
+      elif opt in ('-y', '--year'):
+         year = arg
+   if (year==''):
+       process_data(path, master_path, results)
+   else:
+       process_year(path, master_path, results, year)
    
 if __name__ == "__main__":
     main(sys.argv[1:])
 
-#python clean_edgar.py -p '/Users/mikaelapisanileal/Documents/edgar/edger-cleaner/' -r '/Users/mikaelapisanileal/Documents/SASUniversityEdition/myfolders'
+#python clean_edgar.py -p '/Volumes/TOSHIBA EXT/rawEDGAR/' -m '/Volumes/TOSHIBA EXT/master index files/' -r '/Users/mikaelapisanileal/Documents/SASUniversityEdition/myfolders' -y '2014'
