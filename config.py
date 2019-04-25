ac410737f4e21cc660f729017271bd0e0b0f3dec
Mikaela Pisani <mpisani@netlabs.com.uy>
2019-04-25 14:27:29 -0500

    fix config

--- config.py
+++ config.py
@@ -24,33 +24,33 @@ class Config:
         self.master_path=self.config['master_path']
         self.results_path=self.config['results_path']
         
-        def get_transferData():
+        def get_transferData(self):
             return self.transferData
         
-        def get_threshold():
+        def get_threshold(self):
             return self.threshold
         
-        def get_error_code_limit():
+        def get_error_code_limit(self):
             return self.error_code_limit
         
-        def get_output_size_mb():
+        def get_output_size_mb(self):
             return self.output_size_mb
         
-        def get_access_token():
+        def get_access_token(self):
             return self.access_token
         
-        def get_dropbox_folder():
+        def get_dropbox_folder(self):
             return self.dropbox_folder
         
-        def get_rows_master_skip():
+        def get_rows_master_skip(self):
             return self.rows_master_skip
         
-        def get_data_path():
+        def get_data_path(self):
             return self.data_path
         
-        def get_master_path():
+        def get_master_path(self):
             return self.master_path
         
-        def get_results_path():
+        def get_results_path(self):
             return self.results_path
 
